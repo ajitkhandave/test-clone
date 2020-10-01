63f4031c64b5d27a95a966862b0d7f6c5e66a1af
Meet <meet.shah@clairvoyantsoft.com>
2020-10-01 20:08:47 +0530

    EI-6839 DataTable component, Style inherite from Parity and Basic layer with Column, Active Sort and Pagination support.

---
+++ src/main/web/projects/reports/src/app/components/data-table/data-table.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, OnInit, ViewChild } from '@angular/core';
+import { DatatableComponent, TableColumn } from '@swimlane/ngx-datatable';
+import { Pagination } from '../../models/pagination';
+import { Sort } from '../../models/sort';
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss']
+})
+export class DataTableComponent implements OnInit {
+  @Input() sorts: Sort[] = [];
+  @Input() columns: TableColumn[] = [];
+  @ViewChild(DatatableComponent) tableCmp: DatatableComponent;
+  setPage$: EventEmitter<number> = new EventEmitter();
+  pageOffset: number = 0;
+  totalCount: number;
+  masterOrders = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  /**
+   * Pagination page Change event handler. Make an API call to get the new data.
+   * @param pagination Pagination object.
+   */
+  onPageChange(pagination: Pagination): void {
+  }
+
+  onActivate(event) {
+
+  }
+
+  onSelect(event) {
+
+  }
+
+}
--- src/main/web/projects/reports/src/app/components/reports/reports.component.ts
+++ src/main/web/projects/reports/src/app/components/reports/reports.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
+  columns: any[];
+  sorts: any[];
+
   constructor() { }
 
   ngOnInit() {
+    this.columns = [
+      { prop: 'orderNumber', name: 'Order Number', sortable: true },
+      { prop: 'user.userFirstName', name: 'Ordered By', sortable: true },
+      { prop: 'shippingAddress.company', name: 'Customer', sortable: true },
+      {
+        prop: 'createdAt',
+        name: 'Date Created',
+        sortable: true
+      },
+      { prop: 'statusName', name: 'Order Status', sortable: true }
+    ];
+
+    this.sorts = [{ prop: 'orderNumber', dir: 'desc' }];
   }
 
 }
