a0be96782f3941c61c7502f56ad7451a7fcaba51
Meet <meet.shah@clairvoyantsoft.com>
2020-10-06 17:45:20 +0530

    EI-6839 Completed the filter feature.

--- src/main/web/projects/reports/src/app/reports/pop-active-products/pop-active-products.component.ts
+++ src/main/web/projects/reports/src/app/reports/pop-active-products/pop-active-products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
+import { TableConfig } from '../../models/table-config';
 import { ReportService } from '../../services/report.service';
 
 @Component({
@@ -17,9 +18,10 @@ export class PopActiveProductsComponent implements OnInit {
 
   filterForm: FormGroup;
 
-  tableConfig = {
+  tableConfig: TableConfig = {
+    filters: new Subject<boolean>(),
     api: () => this.reportService.fetchOrders(),
-    filters: new BehaviorSubject<{ [key: string]: string }>(null)
+    query: (row) => this.applyQuery(row)
   };
 
   constructor(
@@ -45,14 +47,42 @@ export class PopActiveProductsComponent implements OnInit {
   }
 
   onSearch() {
-    const { value } = this.filterForm;
-    // todo write logic to filter.
-    this.tableConfig.filters.next(null);
+    this.tableConfig.filters.next(true);
   }
 
   clearFilter() {
-    this.filterForm.setValue(null);
-    this.onSearch();
+    this.filterForm.patchValue({
+      itemNumber: '',
+      productName: '',
+      staticConfigurable: ''
+    });
+    this.tableConfig.filters.next(false);
+  }
+
+  /**
+   * Method to write the query and conditions based on the filter.
+   * @param row Row data object.
+   * @return Boolean values, this will be used for filter.
+   */
+  applyQuery(row: any): boolean {
+    const { itemNumber, productName, staticConfigurable } = this.filterForm.value;
+    let itemNumberFilter = true;
+    let productNameFilter = true;
+    let staticConfigurableFilter = true;
+
+    if (itemNumber) {
+      itemNumberFilter = row.itemNumber.includes(itemNumber);
+    }
+
+    if (productName) {
+      productNameFilter = row.itemName.includes(productName);
+    }
+
+    if (staticConfigurable) {
+      staticConfigurableFilter = row.staticConfigurable.includes(staticConfigurable);
+    }
+
+    return itemNumberFilter && productNameFilter && staticConfigurableFilter;
   }
 
 }
