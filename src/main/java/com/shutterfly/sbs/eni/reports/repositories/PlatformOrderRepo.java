2e1a9d5a1bd2f104b6f15425324f861ba6ae35d7
angarg <angarg@shutterfly.com>
2020-10-09 17:24:55 +0530

    EI-6847 - Order Status report Integration

---
+++ src/main/java/com/shutterfly/sbs/eni/reports/repositories/PlatformOrderRepo.java
@@ -0,0 +1,12 @@
+package com.shutterfly.sbs.eni.reports.repositories;
+
+import com.shutterfly.sbs.eni.reports.repositories.model.PlatformOrder;
+
+/**
+ * Repository for Platform Order Model
+ *
+ * @author Anju Garg
+ */
+public interface PlatformOrderRepo extends ExtendedRepository<PlatformOrder, String> {
+
+}
