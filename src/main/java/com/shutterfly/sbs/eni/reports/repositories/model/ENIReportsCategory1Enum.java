d370ed7df8141364799af255db42e7d7ca575556
angarg <angarg@shutterfly.com>
2020-10-21 22:15:40 +0530

    EI-6873 - Onboarding Dashboard Report Modifed for queries and all savers report modfied

---
+++ src/main/java/com/shutterfly/sbs/eni/reports/repositories/model/ENIReportsCategory1Enum.java
@@ -0,0 +1,30 @@
+package com.shutterfly.sbs.eni.reports.repositories.model;
+
+public enum ENIReportsCategory1Enum {
+
+  ONBOARDING_DASHBOARD_STANDARD_BROCHURES_BY_MONTH_REPORT("ONBOARDING DASHBOARD STD BROCHURES BY MONTH REPORT","standardBrochuresByMonthRepo"),
+  ONBOARDING_DASHBOARD_STANDARD_BROCHURES_BY_SEGMENT_REPORT("ONBOARDING DASHBOARD STD BROCHURES BY SEGMENT REPORT","standardBrochuresBySegmentRepo"),
+  ONBOARDING_DASHBOARD_STANDARD_BROCHURES_BY_PRODUCT_REPORT("ONBOARDING DASHBOARD STD BROCHURES BY PRODUCT REPORT","standardBrochuresByProductRepo"),
+  ORDER_DETAILS_REPORT("ORDER DETAILS REPORT","orderDetailsRepo"),
+  ALL_SAVERS_REPORTS_BY_MONTH("ALL SAVERS REPORT BY MONTH","standardBrochuresByMonthRepo"),
+  ALL_SAVERS_REPORTS_BY_SEGMENT("ALL SAVERS REPORT BY SEGMENT","standardBrochuresBySegmentRepo"),
+  ALL_SAVERS_REPORTS_BY_SKU("ALL SAVERS REPORT","allSaversRepo");
+
+  String name;
+  String repository;
+
+  ENIReportsCategory1Enum(String name, String repository) {
+    this.name=  name;
+    this.repository = repository;
+
+  }
+
+  public String getRepository() {
+    return this.repository;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+}
--- src/main/java/com/shutterfly/sbs/eni/reports/repositories/model/ReportNames.java
+++ src/main/java/com/shutterfly/sbs/eni/reports/repositories/model/ReportNames.java
@@ -5,7 +5,6 @@ public enum ReportNames {
   STATUS_ALERT_REPORT("STATUS ALERT REPORT", "platformOrderRepo"),
   MONTHLY_VOLUME_REPORT("MONTHLY VOLUME REPORT", "monthlyVolumeRepo"),
   ORDER_STATUS_REPORT("ORDER STATUS REPORT", "platformOrderRepo"),
-  ALL_SAVERS_REPORT("ALL SAVERS REPORT","allSaversRepo"),
   MEMBER_ENGAGEMENT_REPORT("MEMBER ENGAGEMENT REPORT","allSaversRepo");
 
   String name;
