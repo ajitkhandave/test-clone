1d1b8799860164ae5302e657c41347534283ef43
Meet Shah <meet.shah@shutterfly.com>
2020-11-06 20:18:16 +0530

    EI-6848 All savers report API integration completed.

--- src/main/web/projects/reports/src/app/reports/all-savers-report/all-savers-report.component.ts
+++ src/main/web/projects/reports/src/app/reports/all-savers-report/all-savers-report.component.ts
@@ -1,12 +1,12 @@
 import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { take } from 'rxjs/operators';
 import * as moment from 'moment';
 import { DateRange } from '../../models/date-range';
 import { TableConfig } from '../../models/table-config';
 import { ReportService } from '../../services/report.service';
 import { FilterSelectedValidator } from '../../validators/filter-selected.validator';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-all-savers-report',
@@ -20,15 +20,15 @@ export class AllSaversReportComponent implements OnInit, AfterViewInit {
 
   tableConfig: TableConfig = {
     filters: new Subject<boolean>(),
-    api: () => this.reportService.fetchAllSaversReport().pipe(
-      map(this.filterMasterRows.bind(this))
-    ),
+    api: () => this.dataSource$,
     query: (row) => this.applyQuery(row)
   };
 
   maxDate: string;
   dateRange: Subject<DateRange> = new Subject();
+  dataSource$: BehaviorSubject<any[]> = new BehaviorSubject([]);
   filterForm: FormGroup;
+  bySkuMasterRecords: any[] = [];
 
   constructor(
     private reportService: ReportService,
@@ -52,6 +52,7 @@ export class AllSaversReportComponent implements OnInit, AfterViewInit {
       endDate: new FormControl('')
     }, { validators: FilterSelectedValidator });
     this.maxDate = moment().endOf('y').format('YYYY-MM-DD');
+    this.fetchRecords();
   }
 
   ngAfterViewInit() {
@@ -66,6 +67,7 @@ export class AllSaversReportComponent implements OnInit, AfterViewInit {
   }
 
   onSearch() {
+    this.generateRows();
     this.tableConfig.filters.next(true);
   }
 
@@ -90,21 +92,47 @@ export class AllSaversReportComponent implements OnInit, AfterViewInit {
    * @return Boolean values, this will be used for filter.
    */
   applyQuery(row: any): boolean {
-    let isInRange = true;
     let isSku = true;
-    const { startDate, endDate, sku } = this.filterForm.value;
-    if (startDate && endDate) {
-      isInRange = moment(row.orderDate).isBetween(startDate, endDate, 'day', '[]');
-    }
-
+    const { sku } = this.filterForm.value;
     if (sku) {
       isSku = (row.sku || '').toLowerCase().includes(sku.toLowerCase());
     }
-    return isInRange && isSku;
+    return isSku;
   }
 
-  filterMasterRows(resp) {
-    return resp.BY_SKU;
+  fetchRecords() {
+    this.reportService.fetchAllSaversReport().pipe(
+      take(1)
+    ).subscribe(resp => {
+      this.bySkuMasterRecords = [].concat(resp.BY_SKU);
+      this.onSearch();
+    });
   }
 
+  generateRows() {
+    const { startDate, endDate } = this.filterForm.value;
+    const filteredRows = this.bySkuMasterRecords.filter(row => moment(row.orderDate).isBetween(startDate, endDate, 'day', '[]'));
+    const rows = [];
+    filteredRows.forEach((row) => {
+      let existingRow = rows.find(r => r.sku === row.identity.product);
+      if (!existingRow) {
+        existingRow = {
+          kitsCount: Number(row.kitsCount),
+          ordersPerSku: Number(row.ordersPerSku),
+          quantity: Number(row.quantity),
+          p3Segment: row.p3Segment,
+          productName: row.productName,
+          lineItemId: row.lineItemId,
+          lineComponentId: row.lineComponentId,
+          sku: row.identity.product
+        };
+        rows.push(existingRow);
+      } else {
+        existingRow.kitsCount += Number(row.kitsCount);
+        existingRow.ordersPerSku += Number(row.ordersPerSku);
+        existingRow.quantity += Number(row.quantity);
+      }
+    });
+    this.dataSource$.next(rows);
+  }
 }
