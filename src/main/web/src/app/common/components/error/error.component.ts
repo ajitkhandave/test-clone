8ac5f204953c0224778e559ba58a6cf3cc1b3e33
sbalaji <sbalaji@shutterfly.com>
2020-11-17 15:46:47 -0700

    EI-6938 - Role based restriction

---
+++ src/main/web/src/app/common/components/error/error.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {AppConfig} from "../../service/app.config";
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss']
+})
+export class ErrorComponent implements OnInit {
+
+  public status: string;
+  public loginurl:string;
+
+  constructor(private constant: AppConfig) { }
+
+  ngOnInit() {
+    this.status = 'We’re sorry, you do not have permission to access this page.';
+  }
+
+}
