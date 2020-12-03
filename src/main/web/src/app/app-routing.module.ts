6120cc3223cf5de50ff95b65ef4ce64f05917c80
Meet Shah <meet.shah@shutterfly.com>
2020-12-03 17:57:41 +0530

    Login guard added.

--- src/main/web/src/app/app-routing.module.ts
+++ src/main/web/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ErrorComponent} from "./common/components/error/error.component";
+import { ErrorComponent } from './common/components/error/error.component';
+import { LoginGuard } from './common/service/login.guard';
 
 const routes: Routes = [
   {
     path: 'reports/error',
+    canActivate: [LoginGuard],
     component: ErrorComponent
   }
 ];
