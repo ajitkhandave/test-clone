72c87a90388be206ad760cdb21c9291b265794a0
sbalaji <sbalaji@shutterfly.com>
2020-12-17 10:23:52 -0700

    EI-6976 - Auth project to ENI reports

--- src/main/web/src/app/common/components/error/error.component.ts
+++ src/main/web/src/app/common/components/error/error.component.ts
@@ -9,13 +9,11 @@ import {AppConfig} from "../../service/app.config";
 export class ErrorComponent implements OnInit {
 
   public status: string;
-  public loginurl:string;
 
   constructor(private constant: AppConfig) { }
 
   ngOnInit() {
-    this.status = 'We’re sorry, you do not have permission to access this page. Please login in to ';
-    this.loginurl= this.constant.get('login-url');
+    this.status = 'We’re sorry, you do not have permission to access this page';
   }
 
 }
