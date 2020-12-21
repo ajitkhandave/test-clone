004ec1febc5eee28a9e40bbd3eb42df2134d6767
Meet Shah <meet.shah@shutterfly.com>
2020-12-21 21:02:33 +0530

    EI-6971 Removed the extra columns.

--- src/main/web/src/app/common/service/app.config.ts
+++ src/main/web/src/app/common/service/app.config.ts
@@ -37,9 +37,11 @@ export class AppConfig {
   public roleGuard() {
     if (this.authService.authenticated() && location.pathname.indexOf('auth/error') < 0) {
       const authorities = this.authService.decodeToken().authorities;
-      const allowedRoles = this.config['access-role'].split(',');
-      if (!allowedRoles.some(role => authorities.some(authority => authority === role))) {
-        location.href = '\\' + 'auth/error?status=403';
+      if(this.config['access-role']) {
+        const allowedRoles = this.config['access-role'].split(',');
+        if (!allowedRoles.some(role => authorities.some(authority => authority === role))) {
+          location.href = '\\' + 'auth/error?status=403';
+        }
       }
     }
   }
